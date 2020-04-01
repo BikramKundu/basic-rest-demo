f9deedd574325ec45f06e808a270c9eba7a191de
BikramKundu <vikitikitavi@gmail.com>
2020-04-01 13:37:56 +0530

    2nd Commit : Adding new Rest Resource to show month

--- src/main/java/com/bikram/practice/basicrestdemo/resource/RestResourceDemo.java
+++ src/main/java/com/bikram/practice/basicrestdemo/resource/RestResourceDemo.java
@@ -4,6 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 @RestController
 @RequestMapping("/practice")
 public class RestResourceDemo {
@@ -11,5 +15,12 @@ public class RestResourceDemo {
     @GetMapping("/greeting")
     public String showMessage(){
         return "Hello Everyone";
+    }
+
+    @GetMapping("/month")
+    public String getDay(){
+        Calendar cal = Calendar.getInstance();
+        Date date = new Date(cal.getTimeInMillis());
+        return new SimpleDateFormat("MMMM").format(date);
     }
 }
